71d6e4688ac26748f347fa6cf27c5f21a6a8b67e
Yeranos <ero20064@gmail.com>
2022-12-17 01:36:57 +0400

    example

--- src/chapter2/Example2.java
+++ src/chapter2/Example2.java
@@ -5,7 +5,7 @@ public class Example2 {
         int x = 4;
         for (int i = 0; i < x; i++) {
             for (int j = 0; j <= i; j++) {
-                System.out.print("*  ");
+                System.out.print("o  ");
             }
             System.out.println();
         }
