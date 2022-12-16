234c0dea1274afe19580deeafaac858485760e37
root <root@DESKTOP-M5PVBR0.localdomain>
2022-12-17 01:25:01 +0400

    Initial commit

---
+++ src/chapter2/Example.java
@@ -0,0 +1,10 @@
+package chapter2;
+
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class Example {
+
+    public static void main(String[] args) {
+        System.out.println("The World");
+    }
+}
---
+++ src/chapter2/Example2.java
@@ -0,0 +1,13 @@
+package chapter2;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int x = 4;
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*  ");
+            }
+            System.out.println();
+        }
+    }
+}
