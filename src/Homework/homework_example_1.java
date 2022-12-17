e2b64485836b3198c529bd91ff424522d486e05b
Yeranos <ero20064@gmail.com>
2022-12-17 15:31:26 +0400

    example

---
+++ src/Homework/homework_example_1.java
@@ -0,0 +1,11 @@
+package Homework;
+
+public class homework_example_1{
+    public static void main(String[] args) {
+        int a = 20;
+        System.out.println("a == " + a);
+        a += 80;
+        System.out.println("a + 80 == " + a);
+
+    }
+}
