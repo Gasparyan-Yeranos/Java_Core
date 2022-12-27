6c7545083844a5e9f718935f559536a43cd5f948
Yeranos <ero20064@gmail.com>
2022-12-28 02:45:53 +0400

    homework 3

--- src/homework/homework2/ArrayUtil.java
+++ src/homework/homework2/ArrayUtil.java
@@ -3,28 +3,51 @@ package homework.homework2;
 public class ArrayUtil {
     public static void main(String[] args) {
         int[] array = {48, 1, 98, 10, 30, 42, 66, 20, 9, 71};
-        System.out.print("array: ");
-        for (int i = 0; i < 10; i++) {
-            System.out.print(array[i] + " ");
+        int size = array.length;
+        System.out.print("evens of array: ");
+        for (int i = 0; i < size; i++) {
+            if(array[i]%2==0){
+                System.out.print(array[i] + " ");
+            }
         }
         System.out.println();
-
-
-        int min = array[0];
-        for (int i = 1; i < 10; i++) {
-            if (min > array[i]) {
-                min = array[1];
+        //--------------------------------------------------------------
+        System.out.print("odds of array: ");
+        for (int i = 0; i < size; i++) {
+            if(array[i]%2==1){
+                System.out.print(array[i] + " ");
             }
         }
-        System.out.println("minimum == " + min);
-
-
-        int max = array[0];
-        for (int i = 1; i < 10; i++) {
-            if (max < array[i]) {
-                max = array[i];
+        System.out.println();
+        //--------------------------------------------------------------
+        int evenAmount = 0;
+        for (int i = 0; i < size; i++) {
+            if(array[i]%2==0){
+                evenAmount++;
             }
         }
-        System.out.println("maximum == " + max);
+        //--------------------------------------------------------------
+        System.out.println("evens: " + evenAmount);
+        int oddAmount = 0;
+        for (int i = 0; i < size; i++) {
+            if(array[i]%2==1){
+                oddAmount++;
+            }
+        }
+        System.out.println("odds: " + oddAmount);
+        //--------------------------------------------------------------
+        double average = 0;
+        for (int i = 0; i < 10; i++) {
+            average += array[i];
+        }
+        average = average/size;
+        System.out.println("average: " + average);
+        //--------------------------------------------------------------
+        System.out.println("first element: " + array[0]);
+        //--------------------------------------------------------------
+        System.out.println("last element: " + array[size-1]);
+        //--------------------------------------------------------------
+        System.out.println("middler element: " + array[(size-1)/2]);
+
     }
 }
